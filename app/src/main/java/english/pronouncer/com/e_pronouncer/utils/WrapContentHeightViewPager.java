1f651e19de7103f7cb601bdd8f1569493d29e9c8
Ron <rondaulagupu8@gmail.com>
2018-05-11 17:11:26 +0530

    Added viewPager wrap_content height functionality

---
+++ app/src/main/java/english/pronouncer/com/e_pronouncer/utils/WrapContentHeightViewPager.java
@@ -0,0 +1,34 @@
+package english.pronouncer.com.e_pronouncer.utils;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+class WrapContentHeightViewPager extends ViewPager {
+
+    public WrapContentHeightViewPager(Context context) {
+        super(context);
+    }
+
+    public WrapContentHeightViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
