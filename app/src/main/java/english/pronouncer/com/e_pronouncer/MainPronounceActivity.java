ed8b30a84c87df88c57922d2fd0fd3331e0ffd5e
Ron <rondaulagupu8@gmail.com>
2018-05-11 12:01:14 +0530

    Initial commit

---
+++ app/src/main/java/english/pronouncer/com/e_pronouncer/MainPronounceActivity.java
@@ -0,0 +1,13 @@
+package english.pronouncer.com.e_pronouncer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPronounceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pronounce);
+    }
+}
